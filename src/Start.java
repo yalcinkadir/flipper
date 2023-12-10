2b87204396ff9edcaad2e02adb4618abf5bd3353
yalcinkadir <101977911+yalcinkadir@users.noreply.github.com>
2023-12-10 22:56:40 +0100

    added starter in src

---
+++ src/Start.java
@@ -0,0 +1,9 @@
+// Keine Paketdeklaration hier
+import controller.Flipper;
+
+public class Start {
+    public static void main(String[] args) {
+        Flipper flipper = new Flipper();
+        flipper.showStatusAndAskForCoin();
+    }
+}
